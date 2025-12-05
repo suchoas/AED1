374136a11f41985ce97982637a492143e06f4c20
suchoas <suzan.uchoas@unifesp.br>
2025-12-04 22:29:53 -0300

    Create 1040.c

---
+++ 1040.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    float N1, N2, N3, N4, exame, media, media_final;
+
+    scanf("%f %f %f %f", &N1, &N2, &N3, &N4);
+
+    media = (N1*2 + N2*3 + N3*4 + N4*1) / 10.0;
+
+    printf("Media: %.1f\n", media);
+
+    if (media >= 7.0) {
+        printf("Aluno aprovado.\n");
+    }
+    else if (media < 5.0) {
+        printf("Aluno reprovado.\n");
+    }
+    else {
+        printf("Aluno em exame.\n");
+
+        scanf("%f", &exame);
+
+        printf("Nota do exame: %.1f\n", exame);
+
+        media_final = (media + exame) / 2.0;
+
+        if (media_final >= 5.0)
+            printf("Aluno aprovado.\n");
+        else
+            printf("Aluno reprovado.\n");
+
+        printf("Media final: %.1f\n", media_final);
+    }
+
+    return 0;
+}
