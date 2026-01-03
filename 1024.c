1404c3fa4e0a76a2b151430abb3d4de0af0ac8ad
suchoas <suzan.uchoas@unifesp.br>
2026-01-03 09:10:35 -0300

    Update 1024.c

--- 1024.c
+++ 1024.c
@@ -8,7 +8,8 @@ int main(){
     getchar();
 
     while(N--){
-        gets(linha);
+        fgets(linha, sizeof(linha), stdin);
+        linha[strcspn(linha, "\n")] = '\0';
         M = strlen(linha);
 
         for(i = 0; i < M; i++){
