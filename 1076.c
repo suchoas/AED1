76b52ace90da69bd16444ebcc95f97a31aa36036
suchoas <suzan.uchoas@unifesp.br>
2026-01-06 17:40:12 -0300

    Update 1076.c

--- 1076.c
+++ 1076.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_V 100
+#define MAX 100
 
 int main() {
     int T;
-    int adj[MAX_V][MAX_V]; 
+    int adj[MAX][MAX]; 
     
     if (scanf("%d", &T) != 1) return 0;
 
     while (T--) {
         int inicio, V, A;
         int u, v;
-        int arestas_unicas = 0;
+        int arestas = 0;
 
         scanf("%d", &inicio);
 
         scanf("%d %d", &V, &A);
 
-        for (int i = 0; i < MAX_V; i++) {
-            for (int j = 0; j < MAX_V; j++) {
+        for (int i = 0; i < MAX; i++) {
+            for (int j = 0; j < MAX; j++) {
                 adj[i][j] = 0;
             }
         }
@@ -28,12 +28,12 @@ int main() {
             scanf("%d %d", &u, &v);
 
             if (adj[u][v] == 0) {
-                arestas_unicas++;
+                arestas++;
                 adj[u][v] = 1;
                 adj[v][u] = 1;
             }
         }
-        printf("%d\n", arestas_unicas * 2);
+        printf("%d\n", arestas * 2);
     }
 
     return 0;
